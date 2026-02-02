a38df833b77bf83a7e7e925609d1c712c666c419
katleo rantle <kprantle@gmail.com>
2026-02-02 09:34:31 +0200

    create task repository interface

---
+++ src/main/java/com/katleo/task/repository/TaskRepository.java
@@ -0,0 +1,11 @@
+package com.katleo.task.repository;
+
+import com.katleo.task.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+}
