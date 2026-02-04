538cfe911aa61a03f0d3e26e8e339df408fac35a
katleo rantle <kprantle@gmail.com>
2026-02-04 21:59:04 +0200

    Exception handling     - create ErrorResponseDto class     - create GlobalExceptionHandler class     - handled MethodArgumentNotValidException

---
+++ src/main/java/com/katleo/task/controller/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.katleo.task.controller;
+
+import com.katleo.task.domain.dto.ErrorDto;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleValidationExceptions(MethodArgumentNotValidException ex){
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .orElse("vaildation failed");
+
+        ErrorDto errorDto = new ErrorDto(errorMessage);
+        return new ResponseEntity<>(errorDto,HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/katleo/task/domain/dto/ErrorDto.java
@@ -0,0 +1,4 @@
+package com.katleo.task.domain.dto;
+
+public record ErrorDto(String error) {
+}
