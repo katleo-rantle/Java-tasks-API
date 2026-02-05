2dafd5958924fafff6834a9bbd581771a487fb41
katleo rantle <kprantle@gmail.com>
2026-02-05 10:41:56 +0200

    implemented the updateTask method on the task controller

--- src/main/java/com/katleo/task/controller/TaskController.java
+++ src/main/java/com/katleo/task/controller/TaskController.java
@@ -1,8 +1,10 @@
 package com.katleo.task.controller;
 
 import com.katleo.task.domain.CreateTaskRequest;
+import com.katleo.task.domain.UpdateTaskRequest;
 import com.katleo.task.domain.dto.CreateTaskRequstDto;
 import com.katleo.task.domain.dto.TaskDto;
+import com.katleo.task.domain.dto.UpdateTaskRequstDto;
 import com.katleo.task.domain.entity.Task;
 import com.katleo.task.mapper.TaskMapper;
 import com.katleo.task.service.TaskService;
@@ -12,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/api/v1/tasks")
@@ -36,5 +39,15 @@ public class TaskController {
         List<Task> tasks = taskService.listTask();
         List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
         return ResponseEntity.ok(taskDtos);
+    }
+    @PutMapping(path = "/{taskId}")
+    public ResponseEntity<TaskDto> updateTask(
+            @PathVariable UUID taskId,
+            @Valid @RequestBody UpdateTaskRequstDto updateTaskRequstDto
+    ){
+        UpdateTaskRequest updateTaskRequest = taskMapper.fromDto(updateTaskRequstDto);
+        Task task = taskService.updateTask(taskId, updateTaskRequest);
+        TaskDto taskDto = taskMapper.toDto(task);
+        return ResponseEntity.ok(taskDto);
     }
 }
