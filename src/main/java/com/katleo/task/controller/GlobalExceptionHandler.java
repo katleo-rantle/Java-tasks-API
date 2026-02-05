77ffd9bd856dacc3ec886200d876640e99d93cbe
katleo rantle <kprantle@gmail.com>
2026-02-05 10:57:53 +0200

    added handling for the TaskNotFoundException

--- src/main/java/com/katleo/task/controller/GlobalExceptionHandler.java
+++ src/main/java/com/katleo/task/controller/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.katleo.task.controller;
 
 import com.katleo.task.domain.dto.ErrorDto;
+import com.katleo.task.exception.TaskNotFoundException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.UUID;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,5 +22,12 @@ public class GlobalExceptionHandler {
 
         ErrorDto errorDto = new ErrorDto(errorMessage);
         return new ResponseEntity<>(errorDto,HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorDto> handleTaskNotFoundException(TaskNotFoundException ex){
+        UUID taskNotFoundId = ex.getId();
+        String  errorMessage = String.format("task with id '%s' not found", taskNotFoundId);
+        ErrorDto errorDto = new ErrorDto(errorMessage);
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 }
