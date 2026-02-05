9561a96280e1f615144c33b7c07aac23fa46d4f0
katleo rantle <kprantle@gmail.com>
2026-02-05 08:28:35 +0200

    implemented listTask method on the TaskController

--- src/main/java/com/katleo/task/controller/TaskController.java
+++ src/main/java/com/katleo/task/controller/TaskController.java
@@ -9,10 +9,9 @@ import com.katleo.task.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/tasks")
@@ -31,5 +30,11 @@ public class TaskController {
         Task createdTask = taskService.createTask(taskToCreate);
         TaskDto createdTaskDto = taskMapper.toDto(createdTask);
         return new ResponseEntity<>(createdTaskDto, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<TaskDto>> listTasks(){
+        List<Task> tasks = taskService.listTask();
+        List<TaskDto> taskDtos = tasks.stream().map(taskMapper::toDto).toList();
+        return ResponseEntity.ok(taskDtos);
     }
 }
